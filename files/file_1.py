ba1a29a5ffdb5159f05a2e02645f6c87d59fa49a
mexewex <m.sperling.coaching@hotmail.com>
2024-11-01 11:24:39 +0100

    Fixed f-string

--- files/file_1.py
+++ files/file_1.py
@@ -2,7 +2,7 @@
 
 def greetings(names):
     for name in names:
-        print("Hello {name}!")
+        print(f"Hello {name}!")
 
 student_names = ["Pax", "Reuben", "Isaaiah", "Ariel", "Loretta"]
 
