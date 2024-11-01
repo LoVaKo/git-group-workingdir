38fd8396da535b7ad3e7280ca0258703022d442e
Lotte Govers <lottevankoolwijk@hotmail.com>
2024-11-01 11:34:47 +0100

    Add goodbye function and called it with student_names

--- files/file_1.py
+++ files/file_1.py
@@ -2,9 +2,14 @@
 
 def greetings(names):
     for name in names:
-        print("Hello {name}!")
+        print(f"Hello {name}!")
+
+def goodbye(names):
+    for name in names:
+        print(f"Goodbye {name}!")
 
 student_names = ["Pax", "Reuben", "Isaaiah", "Ariel", "Loretta"]
 
 greetings(student_names)
+goodbye(student_names)
 
